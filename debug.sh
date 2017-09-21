70e744ceeffe466cce2c3723037302062a005579
James Kyburz <james.kyburz@gmail.com>
2017-09-21 21:47:44 +0200

    colors

--- debug.sh
+++ debug.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
+# https://raw.githubusercontent.com/JamesKyburz/docker-node-debug/master/debug.sh
+
 service=$1
 
+declare -r cs_color_green="\033[1;92m"
+declare -r cs_color_red="\033[0;91m"
+declare -r cs_color_reset="\033[0m"
+
+alias log_success="log green "
+alias log_error="log red "
+
+shopt -s expand_aliases
+
+log () {
+  local log_color="cs_color_${1}"
+  echo -e "${cs_color_reset}${!log_color}${*:2}${cs_color_reset}"
+}
+
 if [ -z $service ]; then
-  echo "parameter 1 must be service name"
-  echo "optional parameter 2 is node working directory"
-  echo "optional parameter 3 is entry point for node"
+  log_error "parameter 1 must be service name"
+  log_error "optional parameter 2 is node working directory"
+  log_error "optional parameter 3 is entry point for node"
   exit 1
 fi
 
@@ -36,10 +52,10 @@ EOF
 
 debugservice
 
-echo "open chrome with url chrome://inspect, then select inspect for $service"
+log_success "open chrome with url chrome://inspect, then select inspect for $service"
 
 while true; do
-  echo "press any key to stop debugging press (r to restart debug)"
+  log_success "press any key to stop debugging press (r to restart debug)"
   read -n 1 key
   if [ "$key" == "r" ]; then
     removeservice
@@ -50,6 +66,6 @@ while true; do
 done
 rm -rf docker-compose.debug.yml
 echo ""
-echo "restarting $service without debug"
+log_success "restarting $service without debug"
 removeservice
 docker-compose up -d --build $service
